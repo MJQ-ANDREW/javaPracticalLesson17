2a222de209a37cf8eea77a54e1f03a0b3ebac34a
MJQ-ANDREW <andreysa4uk@gmail.com>
2023-12-20 11:31:22 +0300

    Initial commit

---
+++ src/mirea/javaLessons/practical17/EmployeeView.java
@@ -0,0 +1,10 @@
+package mirea.javaLessons.practical17;
+
+public class EmployeeView {
+    public void printEmployeeDetails(Employee employee) {
+        System.out.println("Имя сотрудника: " + employee.getName());
+        System.out.println("Почасовая ставка: $" + employee.getHourlyRate());
+        System.out.println("Отработанные часы: " + employee.getHoursWorked());
+        System.out.println("Заработная плата: $" + employee.calculateSalary());
+    }
+}
---
+++ src/mirea/javaLessons/practical17/Main.java
@@ -0,0 +1,16 @@
+package mirea.javaLessons.practical17;
+
+public class Main {
+    public static void main(String[] args) {
+        Employee employee = new Employee("Иван", 20.0, 40);
+        EmployeeView view = new EmployeeView();
+        EmployeeController controller = new EmployeeController(employee, view);
+
+        controller.updateEmployeeView();
+
+        controller.setEmployeeHourlyRate(30);
+        controller.setEmployeeHoursWorked(50);
+
+        controller.updateEmployeeView();
+    }
+}
